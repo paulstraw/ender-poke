44f4cf0410c34d07e9f33987f00a5fbc391d11f4
Paul Straw <paulstraw@paulstraw.com>
2011-09-04 15:03:48 -0700

    First commit

---
+++ ender.poke.js
@@ -0,0 +1,55 @@
+!function($) {
+	$.ender({
+		poke: function(events) {
+			return this.forEach(function(el) {
+				var t = $(el),
+					gestures,
+					touch = {},
+					lastSwipe;
+
+				function swipeDirection(x1, x2, y1, y2){
+					var xDelta = Math.abs(x1 - x2), yDelta = Math.abs(y1 - y2);
+					if (xDelta >= yDelta) {
+						return (x1 - x2 > 0 ? 'W' : 'E');
+					} else {
+						return (y1 - y2 > 0 ? 'N' : 'S');
+					}
+				}
+
+				t.bind(events).bind({
+					touchstart: function(e) {
+						//reset stuff
+						gestures = [];
+						lastSwipe = '';
+
+						touch.x1 = e.touches[0].pageX;
+						touch.y1 = e.touches[0].pageY;
+					},
+					touchmove: function(e) {
+						touch.x2 = e.touches[0].pageX;
+						touch.y2 = e.touches[0].pageY;
+
+						if (Math.abs(touch.x1 - touch.x2) > 60 || Math.abs(touch.y1 - touch.y2) > 60) {
+							var currentSwipe = swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2);
+
+							touch.x1 = touch.x2;
+							touch.y1 = touch.y2;
+
+							if (currentSwipe == lastSwipe) {
+								return;
+							} else {
+								lastSwipe = currentSwipe;
+								gestures.push(currentSwipe);
+							}
+						}
+					},
+					touchend: function() {
+						if (!gestures.length) return;
+
+						t.trigger(gestures.join('-'));
+					}
+				});
+			});
+		}
+	}, true);
+}(ender);
---
+++ readme.md
@@ -0,0 +1,39 @@
+Ender Poke
+============
+
+A tiny (516 bytes minified) [Ender](http://ender.no.de) module for handling swipe gestures on mobile devices. Currently, the four cardinal directions are supported. To use it, add it to your Ender build (see below) and do something like this:
+
+``` javascript
+$('#someElement').poke({
+	'N-S': function() {
+		//do stuff on "north, then south" swipe
+	},
+	'W': function() {
+		//do stuff on "west" swipe
+	},
+	'S-E-N': function() {
+		//do stuff on "south, east, then north" swipe
+	}
+});
+```
+
+Ender
+-------
+
+Poke is an NPM package that requires Ender. If you're not already using Ender, you're missing out; get it now:
+
+```
+$ npm install ender -g
+```
+
+If you're new to Ender, this will get you going:
+
+```
+$ ender build ender-poke
+```
+
+If you're already running Ender, just add it:
+
+```
+$ ender add ender-poke
+```
